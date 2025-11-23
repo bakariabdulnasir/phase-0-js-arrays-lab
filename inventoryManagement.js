dd5e5ca654057b3c7d05e27c24dc9f455da31eed
bakari <anbphones@icloud.com>
2025-11-23 12:53:58 +0300

    this is my la on inventory management system

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,33 @@
 // Write your code here
+// object declaration 
+let products = ["Laptop", "Phone", "Headphones", "Monitor"]; 
+//logFirstProduct function
 
+function logFirstProduct() {
+    console.log(products[0]);
+
+   
+}
+//addProduct function
+function addProduct(productName) {
+    products.push(productName);
+}
+//updateProduct function
+function updateProductName(position, newName) {
+    products[position] = newName;
+}
+//removeProduct function
+function removeLastProduct() {
+    products.pop();
+}
 
 
 // Export the necessary parts for testing
+// Export for testing
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct,
   products
 };
